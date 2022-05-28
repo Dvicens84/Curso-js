263b2642f756b7eace5bd0b626ac0f59e627791c
Damian Vicens <Damian.Vicens@fravega.com.ar>
2022-05-28 14:36:16 -0300

    entrega de tp

---
+++ entrega.js
@@ -0,0 +1,37 @@
+let nombre = prompt ("ingresa tu nombre ");
+
+let coma = ", " ;
+let hola = "bienvenido   ";
+let mensaje = hola + nombre ;
+
+alert (mensaje)
+
+
+alert ("Quieres Jugar??")
+
+let juego = prompt ("SI o NO")
+let aceptar = Si = SI = S = si = s 
+if (juego == aceptar )
+{
+    alert ("Adivina el numero")
+
+    
+let num_elejido = prompt ("elije un numero del 1 al 15")
+        
+    while (num_elejido != " " || num_elejido == 3){
+            
+            if (num_elejido == 12 ){
+                alert ("perfecto, has ganado")
+                alert ("Cierra la pagina")
+
+            }
+            else{
+                alert ("Perdiste, Vuelva a Intentarlo ")
+                num_elejido = prompt ("elije un numero del 1 al 15")
+            }
+        }
+ }
+ 
+else {
+            alert ("Nos vemos,   " + nombre)}
+
